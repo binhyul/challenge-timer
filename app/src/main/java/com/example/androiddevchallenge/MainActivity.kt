42564eb77a1359ae5f011ba2e1392939755f87a5
hjcho <hjcho@qualson.com>
2021-03-06 23:53:46 +0900

    refactor: delete wild card

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -20,13 +20,24 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.Button
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -50,102 +61,118 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun MyApp() {
     val mainVm = MainViewModel()
-    val timerIsStart : Boolean? by mainVm.timerState.observeAsState(null)
+    val timerIsStart: Boolean? by mainVm.timerState.observeAsState(null)
 
     Surface(color = MaterialTheme.colors.background) {
         Column {
-            Text(fontSize = 30.sp, text = "FOR TIME",fontWeight = FontWeight.Bold,modifier = Modifier.fillMaxWidth(),textAlign = TextAlign.Center)
-            when(timerIsStart){
-                true ->TimerStartPage(mainVm = mainVm)
+            Text(fontSize = 30.sp, text = "FOR TIME", fontWeight = FontWeight.Bold, modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
+            when (timerIsStart) {
+                true -> TimerStartPage(mainVm = mainVm)
                 false -> TimerSettingPage(mainVm = mainVm)
-                null ->EndTimePage(mainVm = mainVm)
-            }
-        }
-
-    }
-}
-
-@Composable
-fun EndTimePage(mainVm: MainViewModel){
-    Box(contentAlignment = Alignment.Center,modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight()) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(text = "End Time",modifier = Modifier.fillMaxWidth(),textAlign = TextAlign.Center,fontWeight = FontWeight.Bold)
-            Button(onClick = { mainVm.changeTimerState() }, modifier = Modifier.padding(0.dp,40.dp,0.dp,0.dp)) {
-                Text(text = "Reset",fontSize = 20.sp,fontWeight = FontWeight.Bold, modifier = Modifier.padding(4.dp))
+                null -> EndTimePage(mainVm = mainVm)
             }
         }
     }
 }
 
 @Composable
-fun TimerStartPage(mainVm: MainViewModel){
-
-    val min : Int by mainVm.min.observeAsState(0)
-    val sec : Int by mainVm.sec.observeAsState(0)
-
-    Box(contentAlignment = Alignment.Center,modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight()) {
+fun EndTimePage(mainVm: MainViewModel) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(text = "Left Time",modifier = Modifier.fillMaxWidth(),textAlign = TextAlign.Center)
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(0.dp, 20.dp, 0.dp, 0.dp),
-                horizontalArrangement = Arrangement.Center) {
-                Text(text = mainVm.getTime(min) + " : "+  mainVm.getTime(sec),fontSize = 20.sp,fontWeight = FontWeight.Bold,modifier = Modifier
-                    .padding(4.dp))
-            }
-            Button(onClick = { mainVm.changeTimerState() }, modifier = Modifier.padding(0.dp,40.dp,0.dp,0.dp)) {
-                Text(text = "Stop",fontSize = 20.sp,fontWeight = FontWeight.Bold, modifier = Modifier.padding(4.dp))
+            Text(text = "End Time", modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center, fontWeight = FontWeight.Bold)
+            Button(onClick = { mainVm.changeTimerState() }, modifier = Modifier.padding(0.dp, 40.dp, 0.dp, 0.dp)) {
+                Text(text = "Reset", fontSize = 20.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(4.dp))
             }
         }
     }
 }
 
+@Composable
+fun TimerStartPage(mainVm: MainViewModel) {
+
+    val min: Int by mainVm.min.observeAsState(0)
+    val sec: Int by mainVm.sec.observeAsState(0)
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "Left Time", modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(0.dp, 20.dp, 0.dp, 0.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = mainVm.getTime(min) + " : " + mainVm.getTime(sec), fontSize = 20.sp, fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(4.dp)
+                )
+            }
+            Button(onClick = { mainVm.changeTimerState() }, modifier = Modifier.padding(0.dp, 40.dp, 0.dp, 0.dp)) {
+                Text(text = "Stop", fontSize = 20.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(4.dp))
+            }
+        }
+    }
+}
 
 @Composable
-fun TimerSettingPage(mainVm : MainViewModel){
-    val timeSet : List<Int>? by mainVm.timeSet.observeAsState()
-    val selectedTime : Int? by mainVm.selectedTime.observeAsState()
-    val timerExpanded : Boolean  by mainVm.timerExpanded.observeAsState(false)
-    Box(contentAlignment = Alignment.Center,modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight()) {
+fun TimerSettingPage(mainVm: MainViewModel) {
+    val timeSet: List<Int>? by mainVm.timeSet.observeAsState()
+    val selectedTime: Int? by mainVm.selectedTime.observeAsState()
+    val timerExpanded: Boolean by mainVm.timerExpanded.observeAsState(false)
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(text = "As fast as possible for time",modifier = Modifier.fillMaxWidth(),textAlign = TextAlign.Center)
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(0.dp, 20.dp, 0.dp, 0.dp),
-                horizontalArrangement = Arrangement.Center) {
-                Text(text = "Time cap : ",fontSize = 20.sp,fontWeight = FontWeight.Bold, modifier = Modifier.padding(4.dp))
-                Text(text = "${selectedTime} minute",fontSize = 20.sp,fontWeight = FontWeight.Bold,modifier = Modifier
-                    .border(
-                        2.dp,
-                        colorResource(id = R.color.black)
-                    )
-                    .padding(4.dp)
-                    .clickable {
-                        mainVm.timerExpanded()
-                    })
-                Box(modifier = Modifier.wrapContentWidth(),contentAlignment = Alignment.Center) {
+            Text(text = "As fast as possible for time", modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(0.dp, 20.dp, 0.dp, 0.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(text = "Time cap : ", fontSize = 20.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(4.dp))
+                Text(
+                    text = "$selectedTime minute", fontSize = 20.sp, fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .border(
+                            2.dp,
+                            colorResource(id = R.color.black)
+                        )
+                        .padding(4.dp)
+                        .clickable {
+                            mainVm.timerExpanded()
+                        }
+                )
+                Box(modifier = Modifier.wrapContentWidth(), contentAlignment = Alignment.Center) {
                     DropdownMenu(expanded = timerExpanded, onDismissRequest = { mainVm.timerExpanded(true) }) {
                         timeSet?.forEach {
                             DropdownMenuItem(onClick = { mainVm.onSelectTime(it) }) {
-                                Text(text = "${it} minute",fontSize = 20.sp,fontWeight = FontWeight.Bold)
+                                Text(text = "$it minute", fontSize = 20.sp, fontWeight = FontWeight.Bold)
                             }
                         }
                     }
                 }
             }
-            Button(onClick = { mainVm.changeTimerState() }, modifier = Modifier.padding(0.dp,40.dp,0.dp,0.dp)) {
-                Text(text = "Start",fontSize = 20.sp,fontWeight = FontWeight.Bold, modifier = Modifier.padding(4.dp))
+            Button(onClick = { mainVm.changeTimerState() }, modifier = Modifier.padding(0.dp, 40.dp, 0.dp, 0.dp)) {
+                Text(text = "Start", fontSize = 20.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(4.dp))
             }
         }
     }
 }
-
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
@@ -155,10 +182,10 @@ fun LightPreview() {
     }
 }
 //
-//@Preview("Dark Theme", widthDp = 360, heightDp = 640)
-//@Composable
-//fun DarkPreview() {
+// @Preview("Dark Theme", widthDp = 360, heightDp = 640)
+// @Composable
+// fun DarkPreview() {
 //    MyTheme(darkTheme = true) {
 //        MyApp()
 //    }
-//}
+// }
--- app/src/main/java/com/example/androiddevchallenge/MainViewModel.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainViewModel.kt
@@ -1,97 +1,113 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge
 
-import androidx.lifecycle.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
+    private val _timeSet: MutableLiveData<List<Int>> = MutableLiveData(listOf())
+    val timeSet: LiveData<List<Int>> = _timeSet
 
-    private val _timeSet : MutableLiveData<List<Int>> = MutableLiveData(listOf())
-    val timeSet : LiveData<List<Int>> = _timeSet
+    private val _selectedTime: MutableLiveData<Int> = MutableLiveData(1)
+    val selectedTime: LiveData<Int> = _selectedTime
 
-    private val _selectedTime : MutableLiveData<Int> = MutableLiveData(1)
-    val selectedTime : LiveData<Int> = _selectedTime
+    private val _timerExpanded: MutableLiveData<Boolean> = MutableLiveData(false)
+    val timerExpanded: LiveData<Boolean> = _timerExpanded
 
-    private val _timerExpanded : MutableLiveData<Boolean> = MutableLiveData(false)
-    val timerExpanded : LiveData<Boolean> = _timerExpanded
+    private val _timerState: MutableLiveData<Boolean?> = MutableLiveData(false)
+    val timerState: LiveData<Boolean?> = _timerState
 
-    private val _timerState : MutableLiveData<Boolean?> = MutableLiveData(false)
-    val timerState : LiveData<Boolean?> = _timerState
-
-    private val _time :MutableLiveData<Int> = MutableLiveData(0)
-    val min : LiveData<Int > = Transformations.map(_time){
+    private val _time: MutableLiveData<Int> = MutableLiveData(0)
+    val min: LiveData<Int > = Transformations.map(_time) {
         it / 60
     }
 
-    val sec : LiveData<Int > = Transformations.map(_time){
+    val sec: LiveData<Int> = Transformations.map(_time) {
         it % 60
     }
 
-    private lateinit var timerJob : Job
+    private lateinit var timerJob: Job
     init {
-        _timeSet.value =ArrayList<Int>().apply {
-                    for(i : Int in 1..100){
-                        add(i)
-                    }
-                }
+        _timeSet.value = ArrayList<Int>().apply {
+            for (i: Int in 1..100) {
+                add(i)
+            }
+        }
     }
 
-    fun onSelectTime(select : Int){
+    fun onSelectTime(select: Int) {
         _selectedTime.value = select
         _timerExpanded.value = false
     }
 
-    fun timerExpanded(disMiss : Boolean = false){
+    fun timerExpanded(disMiss: Boolean = false) {
         val expanded = timerExpanded.value ?: false
-        _timerExpanded.value = if(disMiss){
-           false
-        }else{
+        _timerExpanded.value = if (disMiss) {
+            false
+        } else {
             !expanded
         }
     }
 
-    fun changeTimerState(){
+    fun changeTimerState() {
         val timerState = timerState.value ?: true
         _timerState.value = !timerState
 
-        if(_timerState.value ==true){
+        if (_timerState.value == true) {
             startTimer()
-        }else{
+        } else {
             stopTimer()
         }
     }
 
-   private fun startTimer(){
-       val selectTime = selectedTime.value ?: 0
-       _time.value = selectTime * 60
-       timerJob = viewModelScope.launch {
-           val time = _time.value ?: return@launch
-           repeat(time+1){
-               _time.value = time - it
-               delay(1000)
-           }
-       }.apply {
-           invokeOnCompletion {
-               if(_timerState.value ==true){
-               _timerState.value = null
-               }
-           }
-       }
+    private fun startTimer() {
+        val selectTime = selectedTime.value ?: 0
+        _time.value = selectTime * 60
+        timerJob = viewModelScope.launch {
+            val time = _time.value ?: return@launch
+            repeat(time + 1) {
+                _time.value = time - it
+                delay(1000)
+            }
+        }.apply {
+            invokeOnCompletion {
+                if (_timerState.value == true) {
+                    _timerState.value = null
+                }
+            }
+        }
 
-       timerJob.start()
-
+        timerJob.start()
     }
 
-    private fun stopTimer(){
+    private fun stopTimer() {
         timerJob.cancel()
     }
 
-    fun getTime(time : Int)= if(time<10){
-        "0${time}"
-    }else{
+    fun getTime(time: Int) = if (time <10) {
+        "0$time"
+    } else {
         "$time"
     }
-
 }
